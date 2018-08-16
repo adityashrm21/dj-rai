7e540e3d3800723112327a69c4309731c69fc565
Cam Sweeney <cpsweene@gmail.com>
2018-08-16 14:00:53 -0700

    clean up script, fix output dir

--- scripts/create_dataset.py
+++ scripts/create_dataset.py
@@ -2,7 +2,7 @@ import os
 import csv
 import re
 from youtube_dl import YoutubeDL
-import logging
+
 
 SCRIPTS = os.path.abspath(os.path.dirname(__file__))
 PROJECT_ROOT = os.path.abspath(os.path.dirname(SCRIPTS))
@@ -10,39 +10,15 @@ DATASETS = os.path.join(PROJECT_ROOT, 'datasets')
 SOURCES = os.path.join(PROJECT_ROOT, 'sources')
 
 
-
-
-SAMPLES = {}
-
-
-
-class MyLogger(object):
-    def debug(self, msg):
-        logging.debug(msg)
-
-    def warning(self, msg):
-        logging.warning(msg)
-
-    def error(self, msg):
-        logging.error(msg)
-
-
-def my_hook(d):
-    if d['status'] == 'finished':
-        logging.info('Done downloading, now converting ...')
-
 YOUTUBE_OPTIONS = {
-    # 'quiet': True,
+    'quiet': True,
     # 'simulate': True,
-    # 'format': 'bestaudio/best',
     'forcefilename': True,
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'mp3',
         # 'preferredquality': '192',
-    }],
-    # 'logger': MyLogger(),
-    'progress_hooks': [my_hook],
+    }]
 }
 
 
@@ -52,9 +28,9 @@ def make_dataset_dir(dataset_name):
     os.makedirs(dataset_dir, exist_ok=True)
     return dataset_dir
 
-def make_dataset_sample_dir(dataset_name, song_title):
+def make_dataset_sample_dir(dataset_name, sample_title):
     dataset_name = os.path.splitext(source_csv)[0]
-    dataset_dir = os.path.join(DATASETS, dataset_name, song_title)
+    dataset_dir = os.path.join(DATASETS, dataset_name, sample_title)
     os.makedirs(dataset_dir, exist_ok=True)
     return dataset_dir
 
@@ -62,40 +38,45 @@ def download_from_csv(source_csv):
     path_tail = os.path.split(source_csv)[1]
     dataset_name = os.path.splitext(path_tail)[0] # kizomba
 
-    og_title_col = "Original Title"
-    og_url_col = "Original URL"
-    remix_title_col = f"{dataset_name.title()} Title"
-    remix_url_col = f"{dataset_name.title()} URL"
-
     dataset_dir = make_dataset_dir(dataset_name)
-    logging.info(f'About to download audio for {dataset_name} dataset to {dataset_dir}')
-
-
+    print(f'About to download audio for {dataset_name} dataset to {dataset_dir}')
 
     with open(source_csv, 'r') as f:
-        reader = csv.DictReader(f)
+        reader = csv.reader(f)
+        next(reader)
         for row in reader:
+            if '-' not in row:
+                download_song(row, dataset_name, remix=False)
+                download_song(row, dataset_name, remix=True)
+            else:
+                print.warn('Skipping row due to missing URL')
 
-            song_title = row[og_title_col]
-            sample_dir = make_dataset_sample_dir(dataset_name, song_title)
+def get_yt_options(dataset, sample_name, remix=False):
+    if remix:
+        outtmpl = os.path.join(dataset, sample_name, '%(title)s-remix.%(ext)s')
+    else:
+        outtmpl = os.path.join(dataset, sample_name, '%(title)s-original.%(ext)s')
 
-            logging.info(f'Downloading {song_title} to {sample_dir}')
-            og_out = os.path.join(dataset_dir, sample_dir, '%(title)s-original.%(ext)s')
-
-            options = {'outtmpl': og_out}
-            options.update(YOUTUBE_OPTIONS)
-            with YoutubeDL(options) as ydl:
-                ydl.download([row[og_url_col]])
-                logging.info('completed original download')
-
-            remix_out = os.path.join(dataset_dir, sample_dir, '%(title)s-remix.%(ext)s')
-            options = {'outtmpl': remix_out}
-            options.update(YOUTUBE_OPTIONS)
-            with YoutubeDL(options) as ydl:
-                ydl.download([row[remix_url_col]])
-                logging.info('completed remix download')
+    options = {'outtmpl': outtmpl}
+    options.update(YOUTUBE_OPTIONS)
+    return options
 
 
+def download_song(row, dataset, remix):
+    if remix:
+        url, title = row[0], row[1]
+    else:
+        url, title = row[2], row[3]
+
+    # use original as dir
+    sample_name = make_dataset_sample_dir(dataset, row[3])
+    print(f'Downloading {title} to {sample_name}')
+
+    options = get_yt_options(dataset, sample_name, remix=remix)
+
+    with YoutubeDL(options) as ydl:
+        ydl.download([url])
+        print('completed download')
 
 if __name__ == '__main__':
     for source_csv in os.listdir(SOURCES):
